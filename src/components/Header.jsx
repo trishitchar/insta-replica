cb1d8e8f747d4abb3cd7a8a9f20149d8388e818d
tchar <trishitchar@gmail.com>
2024-04-26 23:54:35 +0530

    added Modal function

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,19 @@
 'use client'
 import Image from 'next/image';
 import Link from 'next/link';
-import React from 'react';
+import React, { useState } from 'react';
 import { signIn,useSession, signOut } from 'next-auth/react';
+import { FiPlusCircle } from "react-icons/fi";
+import Modal from 'react-modal';
+import { HiCamera } from "react-icons/hi";
+import { RxCross1 } from "react-icons/rx";
 
 const Header = () => {
-
-  const handleLogin = async () => {
-    await signIn();
-  };
-
+  const handleLogin = async () => {await signIn();};
+  const handleLogout = async()=>{await signOut();}
   const {data:session} = useSession();
-  console.log(session)
+  const [isOpen,setIsOpen] = useState(false);
 
-  const handleLogout = async()=>{
-    await signOut();
-  }
   return (
     <header className='shadow-sm border-b sticky top-0 bg-white z-30 p-3'>
       <div className='flex justify-between items-center max-w-6xl mx-auto'>
@@ -46,7 +44,10 @@ const Header = () => {
 
         {/* Menu Item */}
           {session ? (
-            <img src={session.user.image} alt='session.user.name' className='h-10 w-10 rounded-full cursor-pointer' onClick={handleLogout}/>
+            <div className='flex items-center justify-center'>
+                <FiPlusCircle className='text-3xl cursor-pointer transform hover:scale-125 transition duration-300 hover:text-blue-500 mx-5' onClick={()=>setIsOpen(true)}/>
+                <img src={session.user.image} alt={`${session.user.name}'s avatar`} className='h-10 w-10 rounded-full cursor-pointer' onClick={handleLogout}/>
+            </div>
           ) : (
               <button
                 className='text-sm font-semibold text-blue-500'
@@ -55,10 +56,25 @@ const Header = () => {
               >
                 <span>Log in</span>
               </button>
-          )
-          
-      }
+          )}
       </div>
+      {
+        isOpen && (
+          <Modal
+            isOpen={isOpen}
+            className='max-w-lg w-[90%] p-6 absolute top-56 left-[50%] transform -translate-x-1/2 bg-white border-2 rounded-md shadow-md'
+            onRequestClose={() => setIsOpen(false)}
+            ariaHideApp={false}
+          >
+            <div className='flex flex-col justify-center items-center h-full'>
+              <HiCamera className='text-4xl text-gray-400 cursor-pointer h-20 w-20 m-auto' />
+              <RxCross1 onClick={() => setIsOpen(false)} className='absolute cursor-pointer text-2xl font-bold top-2 right-2 hover:text-blue-500 transition duration-300 ease-in-out' />
+              <input type='text' maxLength='150' className='m-4 border-none text-center w-full focus:ring-0 outline-none' placeholder='Please enter the caption' />
+              <button className=' w-full text-white bg-blue-500 rounded-md p-2 shadow-md'>Upload Post</button>
+            </div>
+          </Modal>
+        )
+      }
     </header>
   );
 };
